2f28a3cd481bd2e45b4e0a0ffb13d5c6cb4981e7
Bruno Matos <brunomatosbarbosa@hotmail.com>
2025-03-28 13:29:32 -0300

    ajustes na fonte de indicadores

--- src/app/@core/utils/menuLinks.ts
+++ src/app/@core/utils/menuLinks.ts
@@ -22,7 +22,7 @@ function checkRoles(allowedRole: string) : boolean {
 
 
 export const menulinks = [
-  { id: 1, status: checkRoles(environment.allowedRoles.indicadores), name: 'Indicadores', link: '', url: environment.urls.indicadores, icon: 'menu-icone-indicadores.svg', src: 'SEP', menuIcon: '', color:'rgb(8 49 127)  ' },
+  { id: 1, status: checkRoles(environment.allowedRoles.indicadores), name: 'Indicadores', link: '', url: environment.urls.indicadores, icon: 'menu-icone-indicadores.svg', src: 'Indicadores Admin', menuIcon: '', color:'rgb(8 49 127)  ' },
   { id: 2, status: checkRoles(environment.allowedRoles.capitacao), name: 'Captação de Recursos', icon: 'menu-icone-siscap.svg', link: '/pages/capitation', url: '', src: 'Siscap', menuIcon:'', color:'#F09BBE'},
   { id: 3, status: checkRoles(environment.allowedRoles.sas), name: 'Painéis SAS(Sigefes)', link: '', url: environment.urls.sas, icon: 'menu-icone-sas.svg', src: 'Sigefes/SAS Sefaz', menuIcon:'', color:'#0478ce' },
   { id: 4, status: checkRoles(environment.allowedRoles.projetosEstrategicos), name: 'Projetos Estratégicos', link: '/pages/strategicProjects', url: '', icon: 'menu-icone-openpmo.svg', src: 'OpenPMO', menuIcon:'', color:'#44B39B'},

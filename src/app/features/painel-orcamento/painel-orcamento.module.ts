1f5483ed54f46694c202ce98a84c8417e3af34d4
Ronicley Angelo <ronicley8@hotmail.com>
2025-11-03 17:02:27 -0300

    fstyle: Estilizando o gráfico de pizza

--- src/app/features/painel-orcamento/painel-orcamento.module.ts
+++ src/app/features/painel-orcamento/painel-orcamento.module.ts
@@ -10,7 +10,8 @@ import { OrgChartHorizontalComponent } from "./org-chart-bar/org-chart-horizonta
 import { OrgChartVerticalComponent } from "./org-chart-bar/org-chart-vertical/org-chart-vertical.component";
 import { PainelOrcamentoComponent } from "./painel-orcamento.component";
 import { VerticalBarChartModelComponent } from "../strategic-projects/bar-chart-model/vertical-bar-chart-model/vertical-bar-chart-model.component";
-import { OrgChartPieComponent } from "./org-chart-pie/org-chart-pie.component";
+import { FilterComponent } from "../../shared/components/filter/filter.component";
+import { PieChartComponent } from "./org-chart-pie/org-chart-pie.component";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,6 @@ import { OrgChartPieComponent } from "./org-chart-pie/org-chart-pie.component";
     OrgChartHorizontalComponent,
     OrgChartHorizontalComponent,
     OrgChartVerticalComponent,
-    OrgChartPieComponent
   ],
   imports: [
     CommonModule,
@@ -29,7 +29,9 @@ import { OrgChartPieComponent } from "./org-chart-pie/org-chart-pie.component";
     NgxEchartsModule,
     NbSelectModule,
     VerticalBarChartModelComponent,
-    NbIconModule
-],
+    NbIconModule,
+    FilterComponent,
+    PieChartComponent,
+  ],
 })
 export class PainelOrcamentoModule {}

6a41f88d6b2dbaa29bff99c60a27f37e8113751e
Ronicley Angelo <ronicley8@hotmail.com>
2026-09-23 17:58:43 -0300

    fix(remove-permission): indicador

--- src/app/features/budget-panel/budget-panel-routing.module.ts
+++ src/app/features/budget-panel/budget-panel-routing.module.ts
@@ -34,7 +34,6 @@ const routes: Routes = [
         data: {
           dataSource: "Sigefes",
           allowedRoles: [
-            environment.allowedRoles.indicadores,
             environment.allowedRoles.execucaoOrcamentaria
           ],
           allowIndicadoresOrgao: true,
